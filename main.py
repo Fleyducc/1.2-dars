9995a86c2ae7ad7fe347c0bca53ecee983ba16c9
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-08-27 14:48:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+mult = "Madakaskar"
+
+fut = "CrRonaldo"
+
+Mrs = "Mars Shop"
+
+dav = "Angliya"
+
+msh = 1
+
+nom = "Mersedez Benz"
+
+print(f"men eng yomon ko'rgan multfilm: {mult}, eng yaxshi futbol o'ynaydigan futbolchi: {fut}, Marsda menga yoqadigan eng yaxshi narsa: {Mrs}, kelajakda qaysi davlatlatga sayohat qilishim : {dav}, kelajakda nechta mashinam bo'lishi : {msh}, men eng yaxshi ko'rgan mashina nomi : {nom}")
+
+
+
+son1 = 11.2
+son2 =  33
+son3 = 90.90
+son4 = 1212.12
+son5 = 8899
+
+print(int(son1))
+print(str(son2))
+print(str(son3))
+print(int(son4))
+print(int(son5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = "Mirabbosov Mirsaid"
+# age = 15
+# hasCar = False
+
+# name = str(input("Name:"))
+# age = int(input("Age:"))
+# hasCar = bool(input("hascar:"))
+
+# print(name, type(name)) #string
+# print(age, type(age)) #number
+# print(hasCar, type(hasCar)) #boolean
+
+
+# nomi = str(input("Type:"))
+# ogirligi = int(input("Weight:"))
+# rang = str(input("Rangi:"))
+# yosh = int(input("Yoshi:"))
+# ism = str(input("Ismi:"))
+
+# print(f"Nomi : {nomi}, Og'irligi: {ogirligi}, Rangi: {rang}, Yoshi: {yosh}, Ismi: {ism}")
+
